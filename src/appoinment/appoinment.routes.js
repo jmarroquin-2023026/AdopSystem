4b788d2063dec3f129cb79af7cdcb46d95ee494b
Josue <jmarroquin-2023026@kinal.edu.gt>
2025-02-07 21:45:43 -0600

    Se finaliza el proyecto

--- src/appoinment/appoinment.routes.js
+++ src/appoinment/appoinment.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { AddAppoinment } from "./appoinment.controller.js";
+import { validateJwt } from "../../middlewares/validate.jw.js";
 
 const api=Router();
 
-api.post('/addApoinment', AddAppoinment)
+api.post('/addApoinment',validateJwt, AddAppoinment)
 
 export default api
